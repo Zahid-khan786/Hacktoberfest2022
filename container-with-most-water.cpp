15a171dd092b077687d60ada042da8e119f1e9e5
ZAHID_KHAN <89588409+Zahid-khan786@users.noreply.github.com>
2022-10-29 18:17:13 +0530

    Create container-with-most-water.cpp

---
+++ container-with-most-water.cpp
@@ -0,0 +1,18 @@
+//Name: Zahid Khan
+//Language: C++
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+         int maximum = 0, current;
+	for(int i = 0, j=n-1, k=j; k>0; k--){
+        
+    current = (height[i] < height[j] ?  height[i++] : height[j--])*k;  
+	maximum = max(maximum, current);
+        
+    }
+        
+	return maximum;
+    }   
+};
